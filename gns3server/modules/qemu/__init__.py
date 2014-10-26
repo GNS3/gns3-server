20acca64b5f16f3473548605b6c13f2ee0910bfb
grossmj <grossmj@gns3.net>
2014-10-25 18:01:14 -0600

    Bump version to 1.2.dev1

--- gns3server/modules/qemu/__init__.py
+++ gns3server/modules/qemu/__init__.py
@@ -629,8 +629,9 @@ class Qemu(IModule):
             if "PROGRAMFILES" in os.environ and os.path.exists(os.environ["PROGRAMFILES"]):
                 paths.append(os.path.join(os.environ["PROGRAMFILES"], "qemu"))
         elif sys.platform.startswith("darwin"):
-            # add a specific location on Mac OS X regardless of what's in $PATH
+            # add specific locations on Mac OS X regardless of what's in $PATH
             paths.append("/usr/local/bin")
+            paths.append("/opt/local/bin")
         for path in paths:
             try:
                 for f in os.listdir(path):
