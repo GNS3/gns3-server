bd54c55ae1ea96af545b133bcad5bb7e930267b4
Julien Duponchelle <julien@duponchelle.info>
2015-11-03 12:34:22 +0100

    Fix minor errors reported by codacy.com

--- tests/handlers/test_upload.py
+++ tests/handlers/test_upload.py
@@ -144,10 +144,10 @@ def test_backup_images(server, tmpdir, loop):
     tar.close()
 
     assert os.path.exists(os.path.join('QEMU', 'a.img'))
-    open(os.path.join('QEMU', 'a.img')).read() == 'hello'
+    assert open(os.path.join('QEMU', 'a.img')).read() == 'hello'
 
     assert os.path.exists(os.path.join('QEMU', 'b.img'))
-    open(os.path.join('QEMU', 'b.img')).read() == 'world'
+    assert open(os.path.join('QEMU', 'b.img')).read() == 'world'
 
 
 def test_backup_projects(server, tmpdir, loop):
@@ -176,7 +176,7 @@ def test_backup_projects(server, tmpdir, loop):
     tar.close()
 
     assert os.path.exists(os.path.join('a', 'a.gns3'))
-    open(os.path.join('a', 'a.gns3')).read() == 'hello'
+    assert open(os.path.join('a', 'a.gns3')).read() == 'hello'
 
     assert os.path.exists(os.path.join('b', 'b.gns3'))
-    open(os.path.join('b', 'b.gns3')).read() == 'world'
+    assert open(os.path.join('b', 'b.gns3')).read() == 'world'
