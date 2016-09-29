74dd212def7725bf7dcc443ea1165291f334d74f
Julien Duponchelle <julien@gns3.net>
2016-09-29 15:07:17 +0200

    One less exit deadlock

--- gns3server/controller/project.py
+++ gns3server/controller/project.py
@@ -531,7 +531,7 @@ class Project:
             try:
                 yield from compute.post("/projects/{}/close".format(self._id))
             # We don't care if a compute is down at this step
-            except (aiohttp.errors.ClientOSError, aiohttp.web.HTTPError):
+            except (aiohttp.errors.ClientOSError, aiohttp.web.HTTPError, aiohttp.ClientResponseError):
                 pass
         self._cleanPictures()
         self._status = "closed"
