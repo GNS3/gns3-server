b85b4b73cc8221b7296faaf50fa4509130eb306b
Julien Duponchelle <julien@gns3.net>
2016-11-02 10:22:36 +0100

    Fix a crash when vboxmanage is not found
    Fix #755

--- gns3server/compute/virtualbox/__init__.py
+++ gns3server/compute/virtualbox/__init__.py
@@ -68,7 +68,7 @@ class VirtualBox(BaseManager):
             else:
                 vboxmanage_path = "vboxmanage"
 
-        if not os.path.isabs(vboxmanage_path):
+        if vboxmanage_path and not os.path.isabs(vboxmanage_path):
             vboxmanage_path = shutil.which(vboxmanage_path)
 
         if not vboxmanage_path:
