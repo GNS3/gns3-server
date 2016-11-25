40263e14ca2d23deea7efeb929662502bcff49c0
Julien Duponchelle <julien@gns3.net>
2016-11-25 17:18:23 +0100

    Catch error if you export a project deleted from disk
    Fix #793

--- gns3server/controller/export_project.py
+++ gns3server/controller/export_project.py
@@ -49,6 +49,9 @@ def export_project(project, temporary_dir, include_images=False, keep_compute_id
 
     z = zipstream.ZipFile(allowZip64=True)
 
+    if not os.path.exists(project._path):
+        raise aiohttp.web.HTTPNotFound(text="The project doesn't exist at location {}".format(project._path))
+
     # First we process the .gns3 in order to be sure we don't have an error
     for file in os.listdir(project._path):
         if file.endswith(".gns3"):
