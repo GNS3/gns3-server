1d1861fe51578a50c7c07c5e8b737bf85053c964
Julien Duponchelle <julien@gns3.net>
2016-12-08 10:33:09 +0100

    2.0.0dev6

--- gns3server/version.py
+++ gns3server/version.py
@@ -23,5 +23,5 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.0.0b1"
+__version__ = "2.0.0dev6"
 __version_info__ = (2, 0, 0, -99)
