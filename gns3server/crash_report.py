2acd6ce884e85ddc5be34e4fe9d91c1a6a15eafd
Julien Duponchelle <julien@gns3.net>
2016-12-20 11:57:34 +0100

    2.0.0 beta 2

--- gns3server/crash_report.py
+++ gns3server/crash_report.py
@@ -53,7 +53,7 @@ class CrashReport:
     Report crash to a third party service
     """
 
-    DSN = "sync+https://7f26962042804f8fb586600e9b54f954:d94785a63265426bb4b2e3ecce0b8972@sentry.io/38482"
+    DSN = "sync+https://d1ef96ccbfb54830aa1afaec870f2ee8:860a204699e84f79a319cc943a2d7a85@sentry.io/38482"
     if hasattr(sys, "frozen"):
         cacert = get_resource("cacert.pem")
         if cacert is not None and os.path.isfile(cacert):
