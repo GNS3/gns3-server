9c7d2e9915d34e556a203fdcdf25b09a44f864ba
Julien Duponchelle <julien@gns3.net>
2017-02-06 15:05:29 +0100

    Raise an error if you put an invalid key in node name
    Fix https://github.com/GNS3/gns3-gui/issues/1833

--- gns3server/controller/project.py
+++ gns3server/controller/project.py
@@ -289,7 +289,10 @@ class Project:
         if '{0}' in base_name or '{id}' in base_name:
             # base name is a template, replace {0} or {id} by an unique identifier
             for number in range(1, 1000000):
-                name = base_name.format(number, id=number, name="Node")
+                try:
+                    name = base_name.format(number, id=number, name="Node")
+                except KeyError as e:
+                    raise aiohttp.web.HTTPConflict(text="{" + e.args[0] + "} is not a valid replacement string in the node name")
                 if name not in self._allocated_node_names:
                     self._allocated_node_names.add(name)
                     return name
