a730fce5143429d6cec9bd42ac1fa18f42f10ac5
Julien Duponchelle <julien@gns3.net>
2017-02-16 11:23:19 +0100

    2.0.0dev9

--- gns3server/version.py
+++ gns3server/version.py
@@ -23,7 +23,7 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.0.0b4"
+__version__ = "2.0.0dev9"
 
 # If it's a git checkout try to add the commit
 if "dev" in __version__:
