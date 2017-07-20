0449a5b4ee6dee8b7b3c8e7578d1739cd465539e
Julien Duponchelle <julien@gns3.net>
2017-07-20 17:04:50 +0200

    Cleanup

--- gns3server/handlers/api/controller/drawing_handler.py
+++ gns3server/handlers/api/controller/drawing_handler.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import aiohttp
-
 from gns3server.web.route import Route
 from gns3server.controller import Controller
 
