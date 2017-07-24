a0a7b0af109772d2009982f93e743ee8768858b6
Julien Duponchelle <julien@gns3.net>
2017-07-24 10:39:06 +0200

    Sync appliances

--- scripts/sync_appliances.sh
+++ scripts/sync_appliances.sh
@@ -26,4 +26,7 @@ rm -Rf /tmp/gns3-registry
 git clone https://github.com/GNS3/gns3-registry.git /tmp/gns3-registry
 mv /tmp/gns3-registry/appliances gns3server/appliances
 
+git add .
+git commit -m "Sync appliances"
+
 
