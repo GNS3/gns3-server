0aa9ab53d19ef6afa434313774e093d53a9ff33f
grossmj <grossmj@gns3.net>
2018-09-07 09:34:17 +0200

    Import encodings.idna to avoid LookupError when standard library is in a zip file.

--- gns3server/web/web_server.py
+++ gns3server/web/web_server.py
@@ -29,6 +29,10 @@ import functools
 import time
 import atexit
 
+# Import encoding now, to avoid implicit import later.
+# Implicit import within threads may cause LookupError when standard library is in a ZIP
+import encodings.idna
+
 from .route import Route
 from ..config import Config
 from ..compute import MODULES
