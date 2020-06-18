a018c8e1a86d988ca6a9c8eeda1bceb8951d02d6
grossmj <grossmj@gns3.net>
2020-06-18 12:04:15 +0930

    Add pytest-aiohttp to tests_require in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     version=__import__("gns3server").__version__,
     url="http://github.com/GNS3/gns3-server",
     license="GNU General Public License v3 (GPLv3)",
-    tests_require=["pytest", "pytest-capturelog"],
+    tests_require=["pytest", "pytest-capturelog", "pytest-aiohttp"],
     cmdclass={"test": PyTest},
     description="GNS3 server",
     long_description=open("README.rst", "r").read(),
