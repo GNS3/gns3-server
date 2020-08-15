464fd804cebaf3f569d4552ba53b82ab3b87c17c
grossmj <grossmj@gns3.net>
2020-08-15 16:14:16 +0930

    Set default disk interface type to "none". Fail-safe: use "ide" if an image is set but no interface type is configured. Use the HDA disk interface type if none has been configured for HDD.

--- gns3server/schemas/qemu_template.py
+++ gns3server/schemas/qemu_template.py
@@ -111,7 +111,7 @@ QEMU_TEMPLATE_PROPERTIES = {
     "hda_disk_interface": {
         "description": "QEMU hda interface",
         "enum": ["ide", "sata", "nvme", "scsi", "sd", "mtd", "floppy", "pflash", "virtio", "none"],
-        "default": "ide"
+        "default": "none"
     },
     "hdb_disk_image": {
         "description": "QEMU hdb disk image path",
@@ -121,7 +121,7 @@ QEMU_TEMPLATE_PROPERTIES = {
     "hdb_disk_interface": {
         "description": "QEMU hdb interface",
         "enum": ["ide", "sata", "nvme", "scsi", "sd", "mtd", "floppy", "pflash", "virtio", "none"],
-        "default": "ide"
+        "default": "none"
     },
     "hdc_disk_image": {
         "description": "QEMU hdc disk image path",
@@ -131,7 +131,7 @@ QEMU_TEMPLATE_PROPERTIES = {
     "hdc_disk_interface": {
         "description": "QEMU hdc interface",
         "enum": ["ide", "sata", "nvme", "scsi", "sd", "mtd", "floppy", "pflash", "virtio", "none"],
-        "default": "ide"
+        "default": "none"
     },
     "hdd_disk_image": {
         "description": "QEMU hdd disk image path",
@@ -141,7 +141,7 @@ QEMU_TEMPLATE_PROPERTIES = {
     "hdd_disk_interface": {
         "description": "QEMU hdd interface",
         "enum": ["ide", "sata", "nvme", "scsi", "sd", "mtd", "floppy", "pflash", "virtio", "none"],
-        "default": "ide"
+        "default": "none"
     },
     "cdrom_image": {
         "description": "QEMU cdrom image path",
