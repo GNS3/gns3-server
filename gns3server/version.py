b4c7823696abd45284cc868ee5b3b587bb235364
grossmj <grossmj@gns3.net>
2020-09-04 23:13:28 +0930

    Release v2.2.13

--- gns3server/version.py
+++ gns3server/version.py
@@ -23,8 +23,8 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "2.2.13dev1"
-__version_info__ = (2, 2, 13, 99)
+__version__ = "2.2.13"
+__version_info__ = (2, 2, 13, 0)
 
 if "dev" in __version__:
     try:
