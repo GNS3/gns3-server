6beab9433b95d2548e224a012b5d64a141f6f2a1
grossmj <grossmj@gns3.net>
2021-09-14 19:20:10 +0930

    Release v2.2.25

--- gns3server/crash_report.py
+++ gns3server/crash_report.py
@@ -58,7 +58,7 @@ class CrashReport:
     Report crash to a third party service
     """
 
-    DSN = "https://95f189bae52543e38be226cc1de2c8f3:e06825958e234a3e9ae5a81eaa21993d@o19455.ingest.sentry.io/38482"
+    DSN = "https://54d3363bab36489fb0f7cbbdda6ca7c5:9f1012f8aa1547f683e00c0aac9b99f6@o19455.ingest.sentry.io/38482"
     _instance = None
 
     def __init__(self):
