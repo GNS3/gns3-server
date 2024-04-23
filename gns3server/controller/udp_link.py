35b75f0f7f7f1321c33abb61c802da8a49d0c9dc
grossmj <grossmj@gns3.net>
2024-04-23 18:35:36 +0700

    Fix link capture for ATM switch. Fixes https://github.com/GNS3/gns3-gui/issues/3570

--- gns3server/controller/udp_link.py
+++ gns3server/controller/udp_link.py
@@ -189,7 +189,7 @@ class UDPLink(Link):
         :returns: Node where the capture should run
         """
 
-        ALWAYS_RUNNING_NODES_TYPE = ("cloud", "nat", "ethernet_switch", "ethernet_hub")
+        ALWAYS_RUNNING_NODES_TYPE = ("cloud", "nat", "ethernet_switch", "ethernet_hub", "frame_relay_switch", "atm_switch")
 
         for node in self._nodes:
             if node["node"].compute.id == "local" and node["node"].node_type in ALWAYS_RUNNING_NODES_TYPE and node["node"].status == "started":
