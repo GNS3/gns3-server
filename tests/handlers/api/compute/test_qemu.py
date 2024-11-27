af7fbe6bb48a84f34742bdf9fc195af7c0924eed
grossmj <grossmj@gns3.net>
2024-11-27 15:18:49 +1000

    Fix tests

--- tests/handlers/api/compute/test_qemu.py
+++ tests/handlers/api/compute/test_qemu.py
@@ -45,7 +45,7 @@ def fake_qemu_vm(images_dir):
     img_dir = os.path.join(images_dir, "QEMU")
     bin_path = os.path.join(img_dir, "linux载.img")
     with open(bin_path, "w+") as f:
-        f.write("1")
+        f.write("1234567")
     os.chmod(bin_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
     return bin_path
 
@@ -101,7 +101,7 @@ async def test_qemu_create_with_params(compute_api, compute_project, base_params
     assert response.json["project_id"] == compute_project.id
     assert response.json["ram"] == 1024
     assert response.json["hda_disk_image"] == "linux载.img"
-    assert response.json["hda_disk_image_md5sum"] == "c4ca4238a0b923820dcc509a6f75849b"
+    assert response.json["hda_disk_image_md5sum"] == "fcea920f7412b5da7be0cf42b8c93759"
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Not supported on Windows")
@@ -279,7 +279,7 @@ async def test_images(compute_api, fake_qemu_vm):
 
     response = await compute_api.get("/qemu/images")
     assert response.status == 200
-    assert {"filename": "linux载.img", "path": "linux载.img", "md5sum": "c4ca4238a0b923820dcc509a6f75849b", "filesize": 1} in response.json
+    assert {"filename": "linux载.img", "path": "linux载.img", "md5sum": "fcea920f7412b5da7be0cf42b8c93759", "filesize": 7} in response.json
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Does not work on Windows")
