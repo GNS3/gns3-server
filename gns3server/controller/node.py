c820a807ad39e354386bac5f3358fd0ed8a1f792
grossmj <grossmj@gns3.net>
2024-12-17 16:24:42 +0700

    Fix IOU node start

--- gns3server/controller/node.py
+++ gns3server/controller/node.py
@@ -572,13 +572,13 @@ class Node:
         Start a node
         """
         try:
-            # For IOU we need to send the licence everytime (if enabled)
+            # For IOU: we need to send the licence everytime we start a node
             if self.node_type == "iou":
-                license_check = self._project.controller.iou_license.get("license_check")
-                iourc_content = self._project.controller.iou_license.get("iourc_content")
-                if license_check:
-                    data = {"license_check": license_check, "iourc_content": iourc_content}
-            await self.post("/start", data=data, timeout=240)
+                license_check = self._project.controller.iou_license.get("license_check", True)
+                iourc_content = self._project.controller.iou_license.get("iourc_content", None)
+                await self.post("/start", timeout=240, data={"license_check": license_check, "iourc_content": iourc_content})
+            else:
+                await self.post("/start", data=data, timeout=240)
         except asyncio.TimeoutError:
             raise ControllerTimeoutError(f"Timeout when starting {self._name}")
 
