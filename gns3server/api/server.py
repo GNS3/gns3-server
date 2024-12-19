7229707746ad139fdfa43317cac1a3f348ba85fc
grossmj <grossmj@gns3.net>
2024-12-19 17:25:42 +0700

    Use static favicon for API docs. Ref #3674

--- gns3server/api/server.py
+++ gns3server/api/server.py
@@ -104,6 +104,7 @@ async def custom_swagger_ui_html():
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="/static/swagger-ui-bundle.js",
         swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/favicon.ico"
     )
 
 @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
@@ -117,6 +118,7 @@ async def redoc_html():
         openapi_url=app.openapi_url,
         title=app.title + " - ReDoc",
         redoc_js_url="/static/redoc.standalone.js",
+        redoc_favicon_url="/static/favicon.ico"
     )
 
 @app.exception_handler(ControllerError)
