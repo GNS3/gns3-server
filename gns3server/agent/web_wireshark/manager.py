02b2de24fe88f7b9c95fd2eafae5d2333bc003c0
YueGuobin <yueguobin@outlook.com>
2026-04-09 14:46:55 +0800

    style: add GPLv3 headers with copyright to web_wireshark modules
    Co-Authored-By: YueGuobin <yueguobin@gmail.com>

--- gns3server/agent/web_wireshark/manager.py
+++ gns3server/agent/web_wireshark/manager.py
@@ -1,9 +1,25 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Web Wireshark - Session Manager
+# Copyright (C) 2026 YueGuobin
 #
-# This module handles xpra session management for Web Wireshark containers.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Web Wireshark - Session Manager
+
+This module handles xpra session management for Web Wireshark containers.
+"""
 
 import asyncio
 import logging
